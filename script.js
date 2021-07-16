430b3327f007382d34be7387a4d2e49d47eb5d8c
Chris Soncrant <christopher.soncrant@gmail.com>
2021-07-16 12:10:05 -0700

    Added JS code, but it is not working. Moving on to new project for now

---
+++ script.js
@@ -0,0 +1,18 @@
+
+/*
+window.onscroll = scrollFunction();
+
+function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        document.getElementById("header").style.paddingTop = "5px";
+        document.getElementById("tb").style.bottom = "100%";
+    } else {
+        document.getElementById("header").style.paddingTop = "20px";
+        document.getElementById("tb").style.bottom = "0";
+    };
+};
+
+*/
+
+
+
